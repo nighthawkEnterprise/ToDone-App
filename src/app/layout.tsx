0712f6d1d8f04779ff31766df86574ec4f7e412d
Nithin K. Moorthy <nithinmoorthy11@gmail.com>
2025-07-02 21:41:45 -0700

    Add navigation and footer from dev link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { DevLinkProvider } from "@/devlink/DevLinkProvider";
+import { Navigation, Footer } from "@/devlink";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -34,9 +35,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} antialiased`}
       >
         <DevLinkProvider>
-          {/* Add here any Navbar or Header you want to be present on all pages */}
+          <Navigation />
           {children}
-          {/* Add here any Footer you want to be present on all pages */}
+          <Footer />
         </DevLinkProvider>
       </body>
     </html>
